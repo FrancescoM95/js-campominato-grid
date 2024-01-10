ec5baf74ae26630ede9e2d9003d8bbe95de9e901
FrancescoM95 <mon95fran@gmail.com>
2024-01-10 16:58:23 +0100

    milestone 4

--- js/script.js
+++ js/script.js
@@ -25,12 +25,18 @@ const createCell = cellNumber => {
 
 
 // Al click sul bottone viene creata la griglia con le 100 celle
-play.addEventListener('click', function () {
+play.addEventListener('click', () => {
     const grid = document.createElement('div');
     grid.classList.add('grid');
     container.appendChild(grid);
     for (let i = 1; i <= totalCells; i++) {
         const cell = createCell(i);
         grid.appendChild(cell);
+
+        //Al click la cella si colora d'azzurro e stampiamo il numero corrispondente in console
+        cell.addEventListener('click', () => {
+            cell.classList.toggle('clicked');
+            console.log('Il numero della cella è:', i);
+        })
     }
 })
