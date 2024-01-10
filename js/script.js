153e9babc3401d95cb9feb35c6abc08d701ce6e6
FrancescoM95 <mon95fran@gmail.com>
2024-01-10 16:42:18 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 console.log('JS OK');
+
+// Recupero elementi dalla pagina
+
+const play = document.getElementById('play');
+const container = document.getElementById('container');
+
+// Dati di partenza
+
+const rows = 10;
+const cols = 10;
+const totalCells = rows * cols;
+
+
+//* FUNZIONI
+
+//Funzione per creare una cella
+
+const createCell = () => {
+    const newCell = document.createElement('div');
+    newCell.classList.add('cell');
+    return newCell;
+}
+
+
+// Al click sul bottone viene creata la griglia con le 100 celle
+play.addEventListener('click', function () {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    container.appendChild(grid);
+    for (let i = 1; i <= totalCells; i++) {
+        const cell = createCell();
+        grid.appendChild(cell);
+    }
+})
